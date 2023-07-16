fde35ab7e696196bf463f109e7d83e214f70e617
mayphyuoo <mayphyuoo.may@gmail.com>
2023-07-16 23:11:21 +1000

    add rough UI layout

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import { Container, List } from 'semantic-ui-react';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+	const [activities, setActivities] = useState<Activity[]>([]);
+
+	useEffect(() => {
+		axios.get<Activity[]>('http://localhost:5000/api/activities')
+			.then(response => {
+				setActivities(response.data);
+			})
+	}, []);
+
+	return (
+		<React.Fragment>
+			<NavBar />
+			<Container style={{ marginTop: '7em' }}>
+				<ActivityDashboard activities={activities} />
+			</Container>
+		</React.Fragment>
+	);
+}
+
+export default App;
