7afdcac8b5e3e893bb75d709a8756ebdfeb7839c
mayphyuoo <mayphyuoo.may@gmail.com>
2023-07-18 04:22:43 +1000

    implement CRUD operation in react app

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
-import { Container, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
 	const [activities, setActivities] = useState<Activity[]>([]);
@@ -34,6 +35,18 @@ function App() {
 		setEditMode(false);
 	}
 
+	function handleCreateOrEditActivity(activity: Activity) {
+		activity.id ?
+			setActivities([...activities.filter(x => x.id !== activity.id), activity])
+			: setActivities([...activities, {...activity, id: uuid()}]);
+		setEditMode(false);
+		setSelectedActivity(activity);
+	}
+
+	function handleDeleteActivity(id: string) {
+		setActivities([...activities.filter(x => x.id !== id)]);
+	}
+
 	return (
 		<React.Fragment>
 			<NavBar openForm={handleFormOpen} />
@@ -45,7 +58,9 @@ function App() {
 					cancelSelectActivity={handleCancelSelectActivity}
 					editMode={editMode}
 					openForm={handleFormOpen}
-					closeForm={handleFormClose} />
+					closeForm={handleFormClose}
+					createOrEdit={handleCreateOrEditActivity}
+					deleteActivity={handleDeleteActivity} />
 			</Container>
 		</React.Fragment>
 	);
