72244af4e78dd624dc28ea2febf3649374d5a47e
mayphyuoo <mayphyuoo.may@gmail.com>
2023-07-24 22:02:32 +1000

    add axios and connect frontend and API

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props {
     activity: Activity | undefined;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
 
-export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit }: Props) {
+export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit, submitting }: Props) {
 
     const initialState = selectedActivity ?? {
         id: '',
@@ -51,6 +52,7 @@ export default function ActivityForm({ activity: selectedActivity, closeForm, cr
                     onChange={handleInputChange} />
                 <Form.Input
                     placeholder='Date'
+                    type='date'
                     value={activity.date}
                     name='date'
                     onChange={handleInputChange} />
@@ -64,7 +66,7 @@ export default function ActivityForm({ activity: selectedActivity, closeForm, cr
                     value={activity.venue}
                     name='venue'
                     onChange={handleInputChange} />
-                <Button floated='right' positive type='submit' content='Submit' />
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
